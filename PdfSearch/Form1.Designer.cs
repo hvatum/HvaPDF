67693dae9164874a4072dfac4f6c2934916b8305
Stian Hvatum <stian@unisoft.no>
2016-03-03 14:33:04 +0100

    initial version

---
+++ PdfSearch/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PdfSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBoxPath.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBoxPath.Location = new System.Drawing.Point(77, 13);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(355, 20);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Search for:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.Location = new System.Drawing.Point(12, 65);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(501, 186);
+            this.listBoxResults.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 261);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "Form1";
+            this.Text = "PdfSearch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxResults;
+    }
+}
+
---
+++ PdfSearch/Form1.cs
@@ -0,0 +1,133 @@
+﻿/* 
+    PdfSearch quickly searches through all PDFs in a folder for a given text string
+    Copyright(C) 2016  Stian Hvatum
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.If not, see<http://www.gnu.org/licenses/>.
+*/
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBoxPath.Text;
+            DialogResult r = folderBrowserDialog1.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                textBoxPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button2.Text = "Searching...";
+            try
+            {
+
+            listBoxResults.Items.Clear();
+            string path = textBoxPath.Text;
+            string searchFor = textBox1.Text;
+            if (Directory.Exists(path))
+            {
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.pdf");
+                foreach (string file in files)
+                {
+                    List<int> r = searchPdf(file, searchFor);
+                    AddResult(file, r, searchFor);
+                }
+            }
+            else if (File.Exists(path))
+            {
+                List<int> r = searchPdf(path, searchFor);
+                AddResult(path, r, searchFor);
+            }
+            } finally
+            {
+                button2.Text = "Search";
+                button2.Enabled = true;
+            }
+        }
+
+        private void AddResult(string file, List<int> r, string match)
+        {
+            if (r.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(file).Append(": Match for \"").Append(match).Append("\" on page ");
+
+                bool first = true;
+                foreach (int i in r)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append(i);
+                }
+                listBoxResults.Items.Add(sb.ToString());
+            }
+            else
+            {
+                listBoxResults.Items.Add(file + ": No matches for \"" + match + "\"");
+            }
+        }
+
+        private List<int> searchPdf(string file, string searchFor)
+        {
+            
+            searchFor = searchFor.ToUpper();
+            List<int> r = new List<int>();
+            PdfReader reader = new PdfReader(file);
+            
+            for (int i = 1; i < reader.NumberOfPages; i++)
+            {
+                string text = PdfTextExtractor.GetTextFromPage(reader, i);
+                if (text.ToUpper().Contains(searchFor))
+                {
+                    r.Add(i);
+                }
+            }
+            return r;
+        }
+    }
+}
